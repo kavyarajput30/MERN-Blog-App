68a2a8b9c497bc2cc2e2952ff2fa3649ded422bd
kavyarajput30 <rajputkavya2004@gmail.com>
2024-05-20 02:03:34 +0530

    added base config in vitr config

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -9,7 +9,6 @@ import { PersistGate } from "redux-persist/integration/react";
 import ThemeProvider from "./components/ThemeProvider.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  // <React.StrictMode>
   <PersistGate persistor={persistor}>
     <Provider store={store}>
       <BrowserRouter>
