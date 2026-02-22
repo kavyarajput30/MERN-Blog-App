b516ca148d4d0b6d910133435e48a07ffcf7b914
kavya rajput <rajputkavya2004@gmail.com>
2026-02-22 14:44:22 +0530

    Fix server listen address to default behavior

--- server/src/index.js
+++ server/src/index.js
@@ -8,7 +8,7 @@ const port = process.env.PORT || 8080
 
 
 ConnectToDB().then(
-    app.listen(port,'0.0.0.0/0', () => {
+    app.listen(port, () => {
         console.log(`Server is running on port ${port}`)
     })
 ).catch(
